55fe643a9ac726ac668ef82787a300975a1afac5
Ja-e <leeniesta@naver.com>
2022-11-17 23:53:36 +0900

    두잉두잉 클론 - 메모지 할 일 목록, 감정 라디오버튼, 하단 메모 텍스트 필드 추가

--- lib/main.dart
+++ lib/main.dart
@@ -27,14 +27,11 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+enum Emo { A, B, C, D, E, F }
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class _MyHomePageState extends State<MyHomePage> {
+  bool _isChecked = false;
+  Emo? _emo = Emo.A;
 
   @override
   Widget build(BuildContext context) {
@@ -103,49 +100,206 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             SizedBox(
               child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                ),
                 //가장 바깥 네모
                 margin: EdgeInsets.symmetric(vertical: 16.0, horizontal: 48.0),
                 padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 4.0),
                 child: Container(
-                  alignment: Alignment.topRight,
                   //검정테두리 네모
                   width: 1000.0,
                   decoration: BoxDecoration(
                     border: Border.all(color: Colors.black, width: 2.0),
                   ),
-                  child: Column(
+                  child: Column(crossAxisAlignment: CrossAxisAlignment.center,
                       //mainAxisAlignment: MainAxisAlignment.center, // 정렬
                       children: <Widget>[
-                        Container(
-                          // 아이콘 테두리용
-                          width: 24.0,
-                          height: 24.0,
-                          margin: EdgeInsets.all(8.0),
-                          padding: EdgeInsets.all(0.0),
-                          child: IconButton(
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Container(
+                              // 아이콘 테두리용
+                              width: 24.0,
+                              height: 24.0,
+                              margin: EdgeInsets.all(8.0),
                               padding: EdgeInsets.all(0.0),
-                              icon: Icon(Icons.edit, size: 14.0),
-                              color: Colors.black,
-                              onPressed: () {}),
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black, width: 1.8),
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(99.0),
+                              child: IconButton(
+                                  padding: EdgeInsets.all(0.0),
+                                  icon: Icon(Icons.edit, size: 14.0),
+                                  color: Colors.black,
+                                  onPressed: () {}),
+                              decoration: BoxDecoration(
+                                border:
+                                    Border.all(color: Colors.black, width: 1.8),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(99.0),
+                                ),
+                              ),
                             ),
-                          ),
+                          ],
                         ),
                         Container(
-                          alignment: Alignment.center,
+                          margin: EdgeInsets.only(bottom: 24.0),
                           child: Text(
-                            "- Doing Doing -",
+                            "-- Doing Doing --",
                             style:
                                 TextStyle(fontSize: 18.0, color: Colors.grey),
                           ),
                         ),
+                        Container(
+                          margin: EdgeInsets.only(bottom: 24.0),
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Checkbox(
+                                      value: _isChecked,
+                                      onChanged: (value) {
+                                        setState(() {
+                                          _isChecked = value!;
+                                        });
+                                      }),
+                                  Container(
+                                    width: 228,
+                                    decoration: BoxDecoration(
+                                      border: Border(
+                                          bottom:
+                                              BorderSide(color: Colors.black)),
+                                    ),
+                                    child: Text(
+                                      "플러터 스터디",
+                                      style: TextStyle(fontSize: 18.0),
+                                    ),
+                                  )
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Checkbox(
+                                      value: _isChecked,
+                                      onChanged: (value) {
+                                        setState(() {
+                                          _isChecked = value!;
+                                        });
+                                      }),
+                                  Container(
+                                    width: 228,
+                                    decoration: BoxDecoration(
+                                      border: Border(
+                                          bottom:
+                                              BorderSide(color: Colors.black)),
+                                    ),
+                                    child: Text(
+                                      "플러터 스터디",
+                                      style: TextStyle(fontSize: 18.0),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(bottom: 24.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                width: 40,
+                                height: 40,
+                                child: Radio(
+                                  value: Emo.A,
+                                  groupValue: _emo,
+                                  onChanged: (Emo? value) {
+                                    setState(() {
+                                      _emo = value;
+                                    });
+                                  },
+                                ),
+                              ),
+                              SizedBox(
+                                width: 40,
+                                height: 40,
+                                child: Radio(
+                                  value: Emo.B,
+                                  groupValue: _emo,
+                                  onChanged: (Emo? value) {
+                                    setState(() {
+                                      _emo = value;
+                                    });
+                                  },
+                                ),
+                              ),
+                              SizedBox(
+                                width: 40,
+                                height: 40,
+                                child: Radio(
+                                  value: Emo.C,
+                                  groupValue: _emo,
+                                  onChanged: (Emo? value) {
+                                    setState(() {
+                                      _emo = value;
+                                    });
+                                  },
+                                ),
+                              ),
+                              SizedBox(
+                                width: 40,
+                                height: 40,
+                                child: Radio(
+                                  value: Emo.D,
+                                  groupValue: _emo,
+                                  onChanged: (Emo? value) {
+                                    setState(() {
+                                      _emo = value;
+                                    });
+                                  },
+                                ),
+                              ),
+                              SizedBox(
+                                width: 40,
+                                height: 40,
+                                child: Radio(
+                                  value: Emo.E,
+                                  groupValue: _emo,
+                                  onChanged: (Emo? value) {
+                                    setState(() {
+                                      _emo = value;
+                                    });
+                                  },
+                                ),
+                              ),
+                              SizedBox(
+                                width: 40,
+                                height: 40,
+                                child: Radio(
+                                  value: Emo.F,
+                                  groupValue: _emo,
+                                  onChanged: (Emo? value) {
+                                    setState(() {
+                                      _emo = value;
+                                    });
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          width: 180,
+                          child: TextField(
+                            maxLines: null,
+                            textAlign: TextAlign.center,
+                            decoration: InputDecoration(
+                              hintText: "오늘은 어떤 하루였나요?",
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        ),
                       ]),
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.white,
                 ),
               ),
             ),
